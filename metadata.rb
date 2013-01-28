5c6480c8764b52b42e4f80c3e0c53982a76d8dcd
rcbjenkins <rcb-deploy@lists.rackspace.com>
2013-01-28 12:57:57 -0600

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Rackspace US, Inc"
 license          "Apache 2.0"
 description      "Installs and configures monit."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.11"
+version          "1.0.12"
 provides	 "service[monit]"
 
 %w{ ubuntu fedora }.each do |os|
