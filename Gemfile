68072a19c2beada918e45e8d657ebe1ed4cda39b
Christopher H. Laco <claco@chrislaco.com>
2013-05-10 15:50:03 -0400

    Make cookbook 1.8.7 compatable because Jenkins is old and crusty.

--- Gemfile
+++ Gemfile
@@ -16,10 +16,10 @@ group :test do
   gem "thor-foodcritic", "~> 0.2.0"
 
   # Move to 1.0 version after release.
-  gem "test-kitchen", git: "git://github.com/opscode/test-kitchen.git", branch: "1.0"
+  gem "test-kitchen", :git => "git://github.com/opscode/test-kitchen.git", :branch => "1.0"
 
   gem "kitchen-vagrant",   "~> 0.9.0"
 
   # Move to 0.1.1 after release. Using git for region support.
-  gem "kitchen-rackspace", git: "https://github.com/RoboticCheese/kitchen-rackspace.git"
+  gem "kitchen-rackspace", :git => "https://github.com/RoboticCheese/kitchen-rackspace.git"
 end
--- metadata.rb
+++ metadata.rb
@@ -20,64 +20,64 @@ recipe "monit::server",
   "Installs the monit package and configures the monit service"
 
 attribute "monit/poll_interval",
-  description: "The monitor polling interval",
-  default: "60"
+  :description => "The monitor polling interval",
+  :default => "60"
 
 attribute "monit/poll_start_delay",
-  description: "The delay time before starting polling",
-  default: "30"
+  :description => "The delay time before starting polling",
+  :default => "30"
 
 attribute "monit/bind_port",
-  description: "The port to bind monit to",
-  default: "2812"
+  :description => "The port to bind monit to",
+  :default => "2812"
 
 attribute "monit/bind_host",
-  description: "The host/ip to find monit to",
-  default: "0.0.0.0"
+  :description => "The host/ip to find monit to",
+  :default => "0.0.0.0"
 
 attribute "monit/login_user",
-  description: "The monit login user",
-  default: "admin"
+  :description => "The monit login user",
+  :default => "admin"
 
 attribute "monit/login_pass",
-  description: "The monit login password",
-  default: "monit"
+  :description => "The monit login password",
+  :default => "monit"
 
 attribute "monit/allowed_hosts",
-  description: "The list of hosts allowed to connect to monit",
-  default: ["0.0.0.0/0"]
+  :description => "The list of hosts allowed to connect to monit",
+  :default => ["0.0.0.0/0"]
 
 attribute "monit/config_dir",
-  description: "The monit configuration directory path",
-  default: "(/etc|/etc/monit)"
+  :description => "The monit configuration directory path",
+  :default => "(/etc|/etc/monit)"
 
 attribute "monit/conf.d_dir",
-  description: "The monit config.d directory path",
-  default: "node['monit']['config_dir']/(monit.d|config.d)"
+  :description => "The monit config.d directory path",
+  :default => "node['monit']['config_dir']/(monit.d|config.d)"
 
 attribute "monit/service_bin",
-  description: "The platform service management binary",
-  default: "(/sbin/service|/usr/sbin/service)"
+  :description => "The platform service management binary",
+  :default => "(/sbin/service|/usr/sbin/service)"
 
 attribute "monit/config_file",
-  description: "The monit configuration file path",
-  default: "node['monit']['config_dir']/monitrc"
+  :description => "The monit configuration file path",
+  :default => "node['monit']['config_dir']/monitrc"
 
 attribute "monit/notify_email",
-  description: "Enable/Disable email notifications",
-  default: nil
+  :description => "Enable/Disable email notifications",
+  :default => nil
 
 attribute "monit/mail_format/subject",
-  description: "The email notification subject",
-  default: "$SERVICE $EVENT"
+  :description => "The email notification subject",
+  :default => "$SERVICE $EVENT"
 
 attribute "monit/mail_format/from",
-  description: "The email notifiation from address",
-  default: "monit@example.com"
+  :description => "The email notifiation from address",
+  :default => "monit@example.com"
 
 attribute "monit/mail_format/message",
-  description: "The email notification message template",
-  default: <<-EOS
+  :description => "The email notification message template",
+  :default => <<-EOS
 Monit $ACTION $SERVICE at $DATE on $HOST: $DESCRIPTION.
 Yours sincerely,
 monit
