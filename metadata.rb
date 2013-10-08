c3a6fd0a61a3b992c390acdaf37871242192f458
Joe Breu <breu@breu.org>
2013-10-08 17:57:34 +0000

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Rackspace US, Inc"
 license          "Apache 2.0"
 description      "Installs and configures monit."
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "1.0.15"
+version           "4.2.0"
 
 %w{ amazon centos debian fedora oracle redhat scientific ubuntu }.each do |os|
   supports os
