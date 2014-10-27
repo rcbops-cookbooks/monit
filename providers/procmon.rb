rcbops-cookbooks/monit

commit 3fcbfa5a5f9234a77f2cc1c64ac9664c54a920b7

Allow empty settings to disable start/stop program
Partial fix to allow openstack-monitoring take no action and monitor
only for rabbitmq.

Issues rcbops/chef-cookbooks#860 and rcbops/chef-cookbooks#909

providers/procmon.rb
@@ -73,7 +73,9 @@ action :add do
       "service_bin" => service_bin,
       "stop_cmd" => stop_cmd,
       "start_cmd" => start_cmd,
-      "http_checks" => http_checks.sort
+      "http_checks" => http_checks.sort,
+      "start_program" => "#{new_resource.service_bin} #{new_resource.script_name} #{new_resource.start_cmd}".strip,
+      "stop_program" => "#{new_resource.service_bin} #{new_resource.script_name} #{new_resource.stop_cmd}".strip
     )
     action :create
     notifies :restart, "service[monit]", :delayed

templates/default/procmon.erb
@@ -8,8 +8,12 @@ check process <%= @identifier %> pidfile "<%= @pid_file %>"
 <% else %>
 check process <%= @identifier %> matching "<%= @process_name %>"
 <% end %>
-  start program = "<%= @service_bin -%> <%= @script_name %> <%= @start_cmd %>"
-  stop program = "<%= @service_bin -%> <%= @script_name %> <%= @stop_cmd %>"
+<% unless @start_program.to_s == "" %>
+  start program = "<%= @start_program -%>"
+<% end %>
+<% unless @stop_program.to_s == "" %>
+  stop program = "<%= @stop_program -%>"
+<% end %>
 <% @http_checks.each do |check| -%>
   <%= check %>
 <% end -%>

commit a8e9beaaf3c1cfa2e2c3326f5ce4ce53e4fd7afb

Fixes start_program/stop_program being malformed
3fcbfa5 introduced bug whereby service_bin, script_name, start_cmd and
stop_cmd must be explicitly specified by the resource.

This commit requires a resource to specify a script_name otherwise
start_program and stop_program supplied to the template are "".

Issue https://github.com/rcbops/chef-cookbooks/issues/972
Issue https://github.com/rcbops/chef-cookbooks/issues/860

providers/procmon.rb
@@ -26,33 +26,42 @@ action :remove do
 end
 
 action :add do
-  # use default service_bin for the platform if it has not been
-  # set in the provider call
-  if not @new_resource.service_bin.nil?
-    service_bin = new_resource.service_bin
-  else
-    service_bin = node["monit"]["service_bin"]
-  end
-
   # if script_name was not set then default it to ""
-  if not @new_resource.script_name.nil?
-    script_name = new_resource.script_name
-  else
+  if @new_resource.script_name.nil? or @new_resource.script_name == ""
+    service_bin = ""
     script_name = ""
-  end
-
-  # if start_cmd is not defined default to 'start'
-  if not @new_resource.start_cmd.nil?
-    start_cmd = new_resource.start_cmd
+    start_cmd = ""
+    stop_cmd = ""
   else
-    start_cmd = "start"
-  end
+    script_name = new_resource.script_name
+    # use default service_bin for the platform if it has not been
+    # set in the provider call
+    if not @new_resource.service_bin.nil?
+      service_bin = new_resource.service_bin
+    else
+      service_bin = node["monit"]["service_bin"]
+    end
 
-  # if start_cmd is not defined default to 'start'
-  if not @new_resource.stop_cmd.nil?
-    stop_cmd = new_resource.stop_cmd
-  else
-    stop_cmd = "stop"
+    # if script_name was not set then default it to ""
+    if not @new_resource.script_name.nil?
+      script_name = new_resource.script_name
+    else
+      script_name = ""
+    end
+
+    # if start_cmd is not defined default to 'start'
+    if not @new_resource.start_cmd.nil?
+      start_cmd = new_resource.start_cmd
+    else
+      start_cmd = "start"
+    end
+
+    # if start_cmd is not defined default to 'start'
+    if not @new_resource.stop_cmd.nil?
+      stop_cmd = new_resource.stop_cmd
+    else
+      stop_cmd = "stop"
+    end
   end
 
   http_checks = ensure_array(new_resource.http_check).compact.map do |check|
@@ -69,13 +78,9 @@ action :add do
       "identifier" => new_resource.name,
       "process_name" => new_resource.process_name,
       "pid_file" => new_resource.pid_file,
-      "script_name" => script_name,
-      "service_bin" => service_bin,
-      "stop_cmd" => stop_cmd,
-      "start_cmd" => start_cmd,
       "http_checks" => http_checks.sort,
-      "start_program" => "#{new_resource.service_bin} #{new_resource.script_name} #{new_resource.start_cmd}".strip,
-      "stop_program" => "#{new_resource.service_bin} #{new_resource.script_name} #{new_resource.stop_cmd}".strip
+      "start_program" => "#{service_bin} #{script_name} #{start_cmd}".strip,
+      "stop_program" => "#{service_bin} #{script_name} #{stop_cmd}".strip
     )
     action :create
     notifies :restart, "service[monit]", :delayed